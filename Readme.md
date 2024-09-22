a0811334ed57bd666f8fa08c567ff22c575659d8
Sujit Avchar <145844481+sujitavchar@users.noreply.github.com>
2024-09-22 13:36:22 +0530

    Database setup suceesfull

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
 Started new project in MERN
+
+-r dotenv/config  --experimental-json-modules
--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
 import dotenv from "dotenv";
-import mongoose from "mongoose";
-import {DB_NAME} from "./constants.js";
-import express from "express";
+import connectDB from "./db/connection.js";
 
 dotenv.config({
-    path: "./env"
+    path: "./.env"
 });
 
+connectDB();
 
-const app = express();
 
 
-//this ia a js iffy for connecting to database. no need to call or export.
-(async () => {
-    try {
-        await  mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-        app.on("error" , (error)=>{
-            console.log("App not listening");
-            throw error
-        })
+// const app = express();
+// import express from "express";
+
+// //this ia a js iffy for connecting to database. no need to call or export.
+// (async () => {
+//     try {
+//         await  mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+//         app.on("error" , (error)=>{
+//             console.log("App not listening");
+//             throw error
+//         })
         
-        app.listen(process.env.PORT, ()=>{
-            console.log(`App is listening on port ${process.env.PORT}`);
-        })
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//         })
 
-    } catch (error) {
-        console.log(error);
-        throw error;
-    }
-})() // Immediatelt get called here
+//     } catch (error) {
+//         console.log(error);
+//         throw error;
+//     }
+// })() // Immediatelt get called here
