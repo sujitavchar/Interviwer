1f19bbe4de32da216a6a57c6abc89c9fac704533
Sujit Avchar <145844481+sujitavchar@users.noreply.github.com>
2024-09-30 00:00:18 +0530

    created data models

---
+++ src/models/posts.js
@@ -0,0 +1,64 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+ 
+const commentSchema = new Schema({
+    author: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    timestamp: {
+      type: Date,
+      default: Date.now
+    },
+    replies: [
+      {
+        author: String,
+        content: String,
+        timestamp: { type: Date, default: Date.now }
+      }
+    ]
+});
+
+
+const postSchema = new Schema({
+    text : {
+        type: String,
+        required: true,
+        index: true
+    },
+
+    comments : [commentSchema],
+
+    image : {
+        type: String, //cloudinary url
+    },
+    video: {
+        type: String, //cloudinaryurl
+    },
+    likes: {
+        type: Number,
+        default : 0
+    },
+    owner : {
+        type: Schema.Types.ObjectId,
+        ref : "User"
+    },
+    isPublished: {
+        type: Boolean,
+        default: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    }
+
+
+},{timestamps : true});
+
+postSchema.plugin(mongooseAggregatePaginate);
+
+export const Post = mongoose.model("Post", postSchema);
---
+++ src/models/users.js
@@ -0,0 +1,100 @@
+import mongoose,{Schema} from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema({
+    fullName:{
+        type : String,
+        required : true,
+        index : true,
+        trim : true
+    },
+    email:{
+        type : String,
+        required : true,
+        lowercase: true
+    },
+    password : {
+        type : String,
+        required : [true, 'Password is required']
+       
+    },
+    collegeName : {
+        type : String,
+        required : true,
+        index : true,
+        trim : true
+    },
+    companyName : {
+        type : String,
+        required : true,
+        index : true,
+        trim : true
+    },
+    mobileNo : {
+        type : String,
+        required: true,
+    },
+    profileImg : {
+        type: String, // cloudinary url
+    },
+    banner : {
+        type: String , //cloudinary url
+    },
+    savedPosts : [
+        {
+            type: Schema.Types.ObjectId,
+            ref : "Post"
+        }
+    ],
+    likedPosts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Post"
+        }
+    ],
+    refreshToken : {
+        type: String
+    }
+
+},{timestamps: true});
+
+userSchema.pre("save", async function(next){ 
+    if(this.isModified("password")){
+        this.password =await bcrypt(this.password, 2);
+    }
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken  = function(){
+     jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXP
+        }
+    );
+}
+
+userSchema.methods.generateRefreshToken  = function(){
+    jwt.sign(
+       {
+           _id: this._id,
+       },
+       process.env.REFRESH_TOKEN_SECRET,
+       {
+           expiresIn: process.env.REFRESH_TOKEN_EXP
+       }
+   );
+}
+
+
+export const User = mongoose.model("User", userSchema);
