c2176fffa948dcd06112e45987710b48a4c5d290
Sujit Avchar <145844481+sujitavchar@users.noreply.github.com>
2025-03-22 17:14:03 +0530

    Created latestevents component

--- frontend/src/pages/Home/home.js
+++ frontend/src/pages/Home/home.js
@@ -3,26 +3,42 @@ import './home.css';
 import Sidebar from '../../components/sidebar';
 import Postcard from '../../components/postcard';
 import Post from '../../components/post';
-import profileIcon from "../../assets/profile_image_icon.png";
-import temp from '../../assets/temp.png'
+import Latestevents from '../../components/latestevents';
 
+import profileIcon from "../../assets/profile_image_icon.png";
+import temp from '../../assets/temp.png';
 
 const HomePage = () => {
   return (
-    <div className="app-container">
+    <div className="home-container">
         <Sidebar/>
-        <Postcard/>
-        <Post
-            user={{ name: "Robert Hammond", profilePic: profileIcon }}
-            time="20 min"
-            title= "Temporary title"
-            text="My wife prepared a surprise trip for me. Here are some shots from Sri Lanka. kjdsaufhasuhf sopsdg rg raaksjdh oihg asg sdpo h pioshg sadiog dsaiogh [dsaoigh ads[oigh dois h idshg ads[iogi hdsgioihs gpsh poi h iasdhg aso oishg [asiohg sa ghisoadhg [saoigh   haisdhg asdigh s dhaoshdg asghashgo asdoghashgshghasdgjkhaskjgkjshg sajghashgjh sagsdahghasgdhd asghas;kgh as;dlkh a;dsdlkh a;sdlk ;asddlkh ;sadlkh ;adslgkh ;dslgh ❤️"
-            image={temp}
-            likes={230}
-            comments={6}
-            shares={2}
-        />
+        
+        {/* Main Content */}
+        <div className="main-content">
+            <div className="feed">
+                <Postcard/>
+                <Post
+                    user={{ name: "Robert Hammond", profilePic: profileIcon }}
+                    time="20 min"
+                    title= "Temporary title"
+                    text="My wife prepared a surprise trip for me. Here are some shots from Sri Lanka. ❤️"
+                    image={temp}
+                    likes={230}
+                    comments={6}
+                    shares={2}
+                />
+            </div>
 
+            {/* Right Sidebar */}
+            <div className="right-sidebar">
+                <Latestevents/>
+            </div>
+
+            {/* Google Ads Space */}
+            <div className="google-ads">
+                <p>Google Ads Here</p>
+            </div>
+        </div>
     </div>
   );
 };
